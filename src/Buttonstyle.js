af7cea89647933090d85a18643b5a052d9dcf11f
L-Abimanyu <abim7562@gmail.com>
2022-08-25 11:12:18 +0530

    calculator main file

---
+++ src/Buttonstyle.js
@@ -0,0 +1,16 @@
+export const ButtonStyle = {
+    clicks:{
+        
+        padding:'10px 0px 0px 0px',
+        margin:'100px 0px 0px 70%',
+        height:'330px',
+        width:'275px',
+        boxShadow: '6px 6px 6px 9px #dedbdbae',
+        
+      },
+      
+     
+        
+
+      
+    }
